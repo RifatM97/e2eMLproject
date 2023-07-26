8bf4a87d7a308e2a6f45d936a94617d2c8dae981
RifatVF <ricioman.it@gmail.com>
2023-07-26 12:34:14 +0100

    transformation, trainer, prediction pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,8 +5,10 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
-@dataclass #decorator - to define class variable
+@dataclass #decorator - to define class variable so you dont have to create init
 class DataIngestionConfig:  
     # defining output location and name
     train_data_path: str=os.path.join("artifacts", "train.csv") 
@@ -46,6 +48,14 @@ class DataIngestion:
             raise CustomException(e, sys)
     
 if __name__=="__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
 
+    # quick test : Data ingestion -> Data transformation 
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modelTrainer = ModelTrainer()
+    print(modelTrainer.initiate_model_trainer(train_arr, test_arr))
+
+ 
